e4d5c3714c776cd039fcf7fe2f7ccc586b3ce237
Owolabi Muktar <abidemimukty@gmail.com>
2023-01-29 22:00:56 +0100

    Add files via upload

---
+++ Nike-Product-Batashu/script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector(".container");
+const color1 = document.querySelector(".color-1");
+const color2 = document.querySelector(".color-2");
+//const color3 = document.querySelector(".color-3");
+const topText = document.querySelector(".card-top-text");
+
+
+color2.addEventListener("click", () => {
+  container.classList.add("change");
+});
+
+color1.addEventListener("click", () => {
+  container.classList.remove("change");
+});
